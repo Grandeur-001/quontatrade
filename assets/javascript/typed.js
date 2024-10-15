6bd093e46cb15d81765db90eb6373bed554227c5
Grandeur-001 <tamunosa.ben@outlook.com>
2024-10-16 00:42:09 +0100

    qq

---
+++ assets/javascript/typed.js
@@ -0,0 +1,12 @@
+    ////AUTO TYPE////
+    var typed = new Typed('.auto-text', {
+        strings: ['Quonta Trade' + ` `],
+        typeSpeed: 200,
+        backSpeed: 200,
+        loop: true
+    });
+
+ 
+
+
+    
